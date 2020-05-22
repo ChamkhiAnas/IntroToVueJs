f30adccf3b412e3ca4eaf863db537c2a9a2e643f
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-05-22 22:50:11 +0000

    V-bind using

---
+++ main.js
@@ -0,0 +1,12 @@
+var app=new Vue({
+
+    el:"#app",
+    data:{
+        Product:"Nike",
+        Description:"Nikky Adidas",
+        Image:"https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+        Link: "https://twitter.com/home"
+    }
+})
+
+
